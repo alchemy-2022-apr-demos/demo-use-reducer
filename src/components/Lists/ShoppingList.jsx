d9a301b07e3c148f7527bc6f1314d2ce8490e7fd
Logan Barnett <logan@alchemycodelab.com>
2022-10-06 17:01:18 -0700

    move list item management into reducers

--- src/components/Lists/ShoppingList.jsx
+++ src/components/Lists/ShoppingList.jsx
@@ -1,17 +1,26 @@
+import React, { useEffect, useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useList } from '../../state/ListsContext.jsx';
 import ItemForm from './ItemForm.jsx';
 import ListItem from './ListItem.jsx';
 import styles from './ShoppingList.css';
+import { Context as ReducerContext } from '../../reducer-provider.jsx'
+import { getListsAction } from '../../actions/lists.js';
+import { dispatchAddItemToList } from '../../actions/list-items.js';
 
 export function ShoppingList() {
+  const { state, dispatch } = useContext(ReducerContext);
   const { id } = useParams();
-  const { list, addItem, removeItem, buyItem } = useList(id);
+  useEffect(() => {
+    getListsAction(dispatch);
+  }, []);
+  const list = state.lists.find(l => l.id == id)
+  const { removeItem, buyItem } = useList(id);
 
   if (!list) return null;
 
-  const handleAdd = async (item) => {
-    await addItem(item);
+  const handleAdd = (item) => {
+    dispatchAddItemToList(dispatch, id, item);
   };
 
   const handleRemove = async ({ id, description, qty }) => {
